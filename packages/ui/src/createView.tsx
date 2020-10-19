1db5fd8d66757ec3994e4b87971bb5a82a3c6c3a
Remon Nashid <remon.sherin@gmail.com>
2020-10-19 12:04:17 -0700

    Set window size

--- packages/ui/src/createView.tsx
+++ packages/ui/src/createView.tsx
@@ -5,6 +5,12 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { Fonts, View, ErrorFallback } from './components';
 
 export default function createView (Entry: React.ComponentType, rootId = 'root'): void {
+  // @FIXME dirty hack
+  // First thing, we set window size because the default window size is too wide.
+  // There's hardly any clean way to accomplish this, since window size is set initially
+  // via chrome.windows.create, which is invoked by Polkadot's singing request handling.
+  window.resizeTo(328, 600);
+
   const rootElement = document.getElementById(rootId);
 
   if (!rootElement) {
