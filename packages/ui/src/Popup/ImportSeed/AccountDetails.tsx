12f06f79bf7b9ff1fae38c3c5e49e444f51941c5
Remon Nashid <remon.sherin@gmail.com>
2020-10-22 07:45:47 -0700

    Fix account name validation (cont.)

--- packages/ui/src/Popup/ImportSeed/AccountDetails.tsx
+++ packages/ui/src/Popup/ImportSeed/AccountDetails.tsx
@@ -70,13 +70,12 @@ export const AccountDetails: FC<Props> = ({ existingAccount, onBack, onContinue 
           <Box>
             <TextInput inputRef={register({ required: true })}
               name='accountName'
-              placeholder='Enter 4 characters or more' />
+              placeholder='Enter account name' />
             {errors.accountName &&
               <Box>
                 <Text color='alert'
                   variant='b3'>
                   {(errors.accountName as FieldError).type === 'required' && 'Required field'}
-                  {(errors.accountName as FieldError).type === 'minLength' && 'Passwordtooshort'}
                 </Text>
               </Box>
             }
