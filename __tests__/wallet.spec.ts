e4d3d4ee4d81eb492cdb91d20323d91659809065
Remon Nashid <remon.sherin@gmail.com>
2020-11-25 05:01:06 -0800

    Another attempt at headful testing

--- __tests__/wallet.spec.ts
+++ __tests__/wallet.spec.ts
@@ -13,6 +13,7 @@ describe('Wallet', () => {
     browser = await puppeteer.launch({ headless: false,
       executablePath: process.env.PUPPETEER_EXEC_PATH,
       args: [
+        '--no-sandbox', // to get around this issue https://github.com/puppeteer/puppeteer/issues/3698
         `--disable-extensions-except=${pathToExtension}`,
         `--load-extension=${pathToExtension}`
       ] });
