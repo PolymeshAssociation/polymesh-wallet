f1c2ba3b69e0cc5176c14f421ca43b293b8a22c6
Remon Nashid <remon.sherin@gmail.com>
2020-11-26 03:08:05 -0800

    E2E test for seed import process (#60)
    * Initial e2e setup
    
    * A working seed import test
    
    * Remove unnecessary IDs
    
    * Headful github action
    
    * use puppeteer-headful image
    
    * Different image
    
    * Another attempt at headful testing

---
+++ __tests__/wallet.spec.ts
@@ -0,0 +1,79 @@
+import puppeteer from 'puppeteer';
+import path from 'path';
+import { mnemonicGenerate } from '@polkadot/util-crypto';
+
+describe('Wallet', () => {
+  let browser: puppeteer.Browser;
+  let page: puppeteer.Page;
+  let extensionUrl: string;
+
+  beforeAll(async () => {
+    const pathToExtension = path.join(__dirname, '../packages/extension/build');
+
+    browser = await puppeteer.launch({ headless: false,
+      executablePath: process.env.PUPPETEER_EXEC_PATH,
+      args: [
+        '--no-sandbox', // to get around this issue https://github.com/puppeteer/puppeteer/issues/3698
+        `--disable-extensions-except=${pathToExtension}`,
+        `--load-extension=${pathToExtension}`
+      ] });
+
+    const targets = await browser.targets();
+    const backgroundPageTarget: any = targets.find((target) => target.type() === 'background_page');
+
+    if (backgroundPageTarget._targetInfo.url) {
+      const [,, extensionID] = backgroundPageTarget._targetInfo.url.split('/');
+
+      extensionUrl = `chrome-extension://${extensionID}/index.html`;
+      console.log('>>>> extensionUrl', extensionUrl);
+
+      page = await browser.newPage();
+      await page.goto(extensionUrl);
+    } else {
+      throw new Error('Unable to find extension URL');
+    }
+  });
+
+  afterAll(async () => {
+    await page.close();
+    await browser.close();
+  });
+
+  describe('Account creation', () => {
+    describe('Import seed phrase', () => {
+      const accountName = 'Imported From Seed';
+      const accountPass = 'j457fkw72jfg89';
+
+      it('Accept agreement checkboxes', async () => {
+        await page.waitForSelector('input[type=checkbox]');
+
+        await page.evaluate(() => {
+          document.querySelectorAll('input[type=checkbox]').forEach((el) => {
+            if (el.parentElement) {
+              el.parentElement.click();
+            }
+          });
+        });
+      });
+
+      it('Proceed with importing seed phrase', async () => {
+        await (await page.waitForXPath("//button[contains(., 'Restore account with recovery phrase')]")).click();
+      });
+
+      it('Fill import seed form', async () => {
+        const seed = mnemonicGenerate(12);
+
+        await (await page.waitForXPath('//textarea')).type(seed);
+        await (await page.waitForXPath("//button[contains(., 'Continue')]")).click();
+        await (await page.waitForXPath("//input[@placeholder='Enter account name']")).type(accountName);
+        await (await page.waitForXPath("//input[@placeholder='Enter 8 characters or more']")).type(accountPass);
+        await (await page.waitForXPath("//input[@placeholder='Confirm your password']")).type(accountPass);
+        await (await page.waitForXPath("//button[contains(., 'Restore')]")).click();
+      });
+
+      it('Account is displayed in accounts list', async () => {
+        await page.waitForXPath(`//span[text()='${accountName}']`);
+      });
+    });
+  });
+});
