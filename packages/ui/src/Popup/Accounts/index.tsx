177b826448707d048a1efbab241b7a610287c5ca
Remon Nashid <remon.sherin@gmail.com>
2020-12-14 08:08:51 -0800

    An attempt to prevent unnecessary network initializaiton

--- packages/ui/src/Popup/Accounts/index.tsx
+++ packages/ui/src/Popup/Accounts/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, Fragment } from 'react';
 import styled from 'styled-components';
 import { AccountContext, PolymeshContext } from '../../components';
 import AddAccount from './AddAccount';
@@ -38,7 +38,7 @@ export default function Accounts (): React.ReactElement {
           {
             Object.keys(networkLabels).map((_network, index) => {
               return (
-                <>
+                <Fragment key={index}>
                   {/* @ts-ignore */}
                   <MenuItem data={{ networkKey: _network }}
                     key={index}
@@ -46,7 +46,7 @@ export default function Accounts (): React.ReactElement {
                     <Text color='gray.2'
                       variant='b1'>{networkLabels[_network as NetworkName]}</Text>
                   </MenuItem>
-                </>
+                </Fragment>
               );
             })
           }
@@ -60,9 +60,9 @@ export default function Accounts (): React.ReactElement {
   };
 
   const handleNetworkChange = async (event: string, data: {networkKey: NetworkName}) => {
-    console.log(data);
-
-    await setPolyNetwork(data.networkKey);
+    if (!!data.networkKey && data.networkKey !== network) {
+      await setPolyNetwork(data.networkKey);
+    }
   };
 
   const groupAccounts = () => (array:IdentifiedAccount[]) =>
