f5e44d05d5b62172618a3beb5251744bb7e9d27b
Remon Nashid <remon.sherin@gmail.com>
2021-02-12 08:47:45 -0800

    Remove unneeded vars

--- packages/ui/src/components/Name.tsx
+++ packages/ui/src/components/Name.tsx
@@ -1,8 +1,5 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext } from 'react';
 
-import { TextInput } from '../ui';
-import { isNotShorterThan } from '../util/validators';
-import ValidatedInput from './ValidatedInput';
 import { AccountContext } from './';
 
 interface Props {
@@ -17,7 +14,6 @@ interface Props {
 
 export default function Name ({ address, className, isFocused, label, onBlur, onChange, value }: Props): React.ReactElement<Props> {
   const { accounts } = useContext(AccountContext);
-  const isNameValid = useMemo(() => isNotShorterThan(3, 'Account name is too short'), []);
 
   const account = accounts.find((account) => account.address === address);
   const startValue = value || account?.name;
@@ -27,8 +23,6 @@ export default function Name ({ address, className, isFocused, label, onBlur, on
       className={className}
       data-input-name
       defaultValue={startValue}
-      // isFocused={isFocused}
-      // label={label || 'A descriptive name for your account'}
       onBlur={onBlur}
       onChange={(event) => {
         const value = event.target.value;
@@ -36,9 +30,7 @@ export default function Name ({ address, className, isFocused, label, onBlur, on
         console.log('Value', value);
         onChange(value);
       }}
-      // onEnter={onBlur}
       type='text'
-      // validator={isNameValid}
     />
   );
 }
