1b2355127ec42aa57f02e607688ad5b1fd3fe4cf
Wahid Rahim <wahidrahim@gmail.com>
2021-02-18 17:27:37 -0500

    feat: add TextOverflowEllipsis component

---
+++ packages/ui/src/ui/TextOverflowEllipsis/TextOverflowEllipsis.tsx
@@ -0,0 +1,21 @@
+import React, { ComponentProps, PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import { Text } from '@polymathnetwork/extension-ui/ui';
+
+export const EllipsiedText = styled(Text)`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+type Props = PropsWithChildren<{ width: number | string }> & ComponentProps<typeof Text>;
+
+export function TextOverflowEllipsis(props: Props) {
+  const { children, ...textProps } = props;
+
+  return (
+    <EllipsiedText {...textProps}>
+      {children}
+    </EllipsiedText>
+  );
+}
