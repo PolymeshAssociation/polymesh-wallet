a6de42172186db70826c539db2454b187b81b4a8
Remon Nashid <remon.sherin@gmail.com>
2021-02-17 16:26:41 -0800

    Disconnect websockets before either 1) creating new API or 2) closing popup port

--- packages/core/src/api/apiPromise/index.ts
+++ packages/core/src/api/apiPromise/index.ts
@@ -4,14 +4,46 @@ import { networkURLs } from '../../constants';
 import { NetworkName } from '../../types';
 import schema from './schema';
 
+let api: ApiPromise | null = null;
+
 async function apiPromise (n: NetworkName): Promise<ApiPromise> {
+  console.time('isReady');
+
+  if (api) {
+    try {
+      await api.disconnect();
+    } catch (error) {
+      console.error(error);
+    }
+
+    api = null;
+  }
+
   const provider = new WsProvider(networkURLs[n]);
 
-  return (new ApiPromise({
+  api = await ApiPromise.create({
     provider,
     rpc: schema[n].rpc,
     types: schema[n].types
-  })).isReadyOrError;
+  });
+
+  await api.isReadyOrError;
+
+  console.timeEnd('isReady');
+
+  return api;
+}
+
+export async function disconnect (): Promise<void> {
+  if (api) {
+    try {
+      await api.disconnect();
+    } catch (error) {
+      console.error(`Failed to close websocket connection: ${JSON.stringify(error)}`);
+    }
+
+    api = null;
+  }
 }
 
 export default apiPromise;
