77dc2830bd64049f5e41c7f9cd6c0ef818cdb2be
Wahid Rahim <wahidrahim@gmail.com>
2021-02-19 12:33:02 -0500

    fix: use `TextOverflowEllipsis` for displaying name in `AccountHeader`

--- packages/ui/src/ui/TextOverflowEllipsis/TextOverflowEllipsis.tsx
+++ packages/ui/src/ui/TextOverflowEllipsis/TextOverflowEllipsis.tsx
@@ -1,18 +1,16 @@
 import { Text } from '@polymathnetwork/extension-ui/ui';
-import React, { ComponentProps, PropsWithChildren } from 'react';
+import React, { ComponentProps, FC } from 'react';
 import styled from 'styled-components';
 
-export const EllipsiedText = styled(Text)`
+export const EllipsiedText = styled(Text)<{ maxWidth: number | string }>`
+  display: inline-block;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 `;
 
-type Props = PropsWithChildren<{ maxWidth: number | string }> &
-ComponentProps<typeof Text>;
+type Props = { maxWidth: number | string } & ComponentProps<typeof Text>;
 
-export function TextOverflowEllipsis (props: Props) {
-  const { children, ...textProps } = props;
-
-  return <EllipsiedText {...textProps}>{children}</EllipsiedText>;
-}
+export const TextOverflowEllipsis: FC<Props> = ({ children, ...textProps }) => (
+  <EllipsiedText {...textProps}>{children}</EllipsiedText>
+);
