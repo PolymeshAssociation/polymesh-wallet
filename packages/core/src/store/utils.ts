d033fb2c291058b73f88b58e3e1ba9fa55d0c1a5
Remon Nashid <remon.sherin@gmail.com>
2021-02-26 13:02:52 -0800

    Api metadata caching

--- packages/core/src/store/utils.ts
+++ packages/core/src/store/utils.ts
@@ -9,3 +9,35 @@ export function initialFeatureState (): Record<NetworkName, unknown> {
 
   return state;
 }
+
+// "Content-Type: application/json" -d '{"id":"1", "jsonrpc":"2.0", "method": "state_getMetadata", "params":[]}'
+
+export function wssFetch (url: string, method: string, params: any[]) {
+  return new Promise((resolve) => {
+    console.time('wssFetch');
+    const requestData = {
+      id: '1',
+      jsonrpc: '2.0',
+      method,
+      params
+    };
+
+    const body = JSON.stringify(requestData);
+
+    const socket = new WebSocket(url);
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      console.timeEnd('wssFetch');
+
+      resolve(data.result);
+
+      socket.close();
+    };
+
+    socket.onopen = () => {
+      socket.send(body);
+    };
+  });
+}
