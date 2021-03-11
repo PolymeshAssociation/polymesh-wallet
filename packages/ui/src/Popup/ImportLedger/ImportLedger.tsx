4945756ad7157a1f9b13e77984755a7b35ae2136
Wahid Rahim <wahidrahim@gmail.com>
2021-03-11 02:50:17 -0500

    wip: name, password form fields

--- packages/ui/src/Popup/ImportLedger/ImportLedger.tsx
+++ packages/ui/src/Popup/ImportLedger/ImportLedger.tsx
@@ -1,14 +1,17 @@
+import { ErrorMessage } from '@hookform/error-message';
 import settings from '@polkadot/ui-settings';
 import { genesisHash } from '@polymathnetwork/extension-core/constants';
 import { SvgLedgerLogo } from '@polymathnetwork/extension-ui/assets/images/icons';
-import { ActionContext, ActivityContext } from '@polymathnetwork/extension-ui/components/contexts';
+import { Password } from '@polymathnetwork/extension-ui/components';
+import { ActionContext, ActivityContext, PolymeshContext } from '@polymathnetwork/extension-ui/components/contexts';
 import Dropdown from '@polymathnetwork/extension-ui/components/Dropdown';
 import Name from '@polymathnetwork/extension-ui/components/Name';
 import { Status, useLedger } from '@polymathnetwork/extension-ui/hooks/useLedger';
-import { createAccountHardware } from '@polymathnetwork/extension-ui/messaging';
-import { Box, Button, Flex, Header, Text } from '@polymathnetwork/extension-ui/ui';
+import { createAccountHardware, validateAccount } from '@polymathnetwork/extension-ui/messaging';
+import { Box, Button, Flex, Header, Text, TextInput } from '@polymathnetwork/extension-ui/ui';
 import { formatters } from '@polymathnetwork/extension-ui/util';
-import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { FormProvider, useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
 import { TroubleshootGuide } from './TroubleshootGuide';
@@ -18,10 +21,18 @@ interface AccOption {
   value: number;
 }
 
+type FormInputs = {
+  accountName: string;
+  password: string;
+}
+
 const AVAIL: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
 function ImportLedger (): React.ReactElement {
   const genesis = genesisHash;
+
+  const methods = useForm<FormInputs>();
+  const { errors, handleSubmit, register } = methods;
 
   const [accountIndex, setAccountIndex] = useState<number>(0);
   const [addressOffset, setAddressOffset] = useState<number>(0);
@@ -31,6 +42,9 @@ function ImportLedger (): React.ReactElement {
   const ledgerData = useLedger(genesis, accountIndex, addressOffset);
   const { address, error: ledgerError, isLoading: ledgerLoading, isLocked: ledgerLocked, refresh, status, warning: ledgerWarning } = ledgerData;
   const isBusy = useContext(ActivityContext);
+
+  const { polymeshAccounts } = useContext(PolymeshContext);
+  const oneAddress = useMemo(() => polymeshAccounts && polymeshAccounts.length > 0 && polymeshAccounts[0].address, [polymeshAccounts]);
 
   useEffect(() => {
     if (address) {
@@ -67,11 +81,23 @@ function ImportLedger (): React.ReactElement {
   const _onSetAccountIndex = useCallback((value: string) => setAccountIndex(Number(value)), []);
   const _onSetAddressOffset = useCallback((value: string) => setAddressOffset(Number(value)), []);
 
+  const onContinue = async ({ accountName, password }: FormInputs) => {
+    if (!oneAddress) return;
+
+    console.log({ accountName, password });
+
+    const isValidPassword = await validateAccount(oneAddress, password);
+
+    console.log({ oneAddress, isValidPassword });
+  };
+
   return (
-    <Box p='s'>
-      {status !== Status.Ok
-        ? <TroubleshootGuide ledgerStatus={status}
-          refresh={refresh}/>
+    <>
+      {false && status !== Status.Ok
+        ? <Box p='s'>
+          <TroubleshootGuide ledgerStatus={status}
+            refresh={refresh}/>
+        </Box>
         : <>
           <Header headerText='Connect your ledger'
             iconAsset={SvgLedgerLogo}>
@@ -84,27 +110,68 @@ function ImportLedger (): React.ReactElement {
           </Header>
 
           <>
-            <Flex p='8px'>
-              <Flex bg='brandLightest'
-                borderRadius='50%'
-                flex='0 0 40px'
-                height={40}
-                justifyContent='center'>
-                <Text color='brandMain'>
+            <Box p='8px'>
+              <Flex>
+                <Flex bg='brandLightest'
+                  borderRadius='50%'
+                  flex='0 0 40px'
+                  height={40}
+                  justifyContent='center'>
+                  <Text color='brandMain'>
                   ??
-                </Text>
+                  </Text>
+                </Flex>
+                <Flex alignItems='flex-start'
+                  flexDirection='column'
+                  ml='8px'>
+                  <Text color='gray.1'
+                    variant='b2m'>??? ???</Text>
+                  <Text color='gray.2'
+                    variant='b3'>
+                    {address && formatters.toShortAddress(address, { size: 33 })}
+                  </Text>
+                </Flex>
               </Flex>
-              <Flex alignItems='flex-start'
-                flexDirection='column'
-                ml='8px'>
-                <Text color='gray.1'
-                  variant='b2m'>??? ???</Text>
-                <Text color='gray.2'
-                  variant='b3'>
-                  {address && formatters.toShortAddress(address, { size: 33 })}
-                </Text>
-              </Flex>
-            </Flex>
+
+              <FormProvider {...methods}>
+                <form
+                  id='ledgerImport'
+                  onSubmit={handleSubmit(onContinue)}
+                >
+                  <Box mt='m'>
+                    <Box>
+                      <Text color='gray.1'
+                        variant='b2m' >
+                      Account name
+                      </Text>
+                    </Box>
+                    <Box>
+                      <TextInput inputRef={register({ required: 'Account name is required' })}
+                        name='accountName'
+                        placeholder='Enter account name' />
+                      <Box>
+                        <Text color='alert'
+                          variant='b3'>
+                          <ErrorMessage errors={errors}
+                            name='accountName' />
+                        </Text>
+                      </Box>
+                    </Box>
+                  </Box>
+
+                  <Password label='Wallet password'
+                    placeholder='Enter your current wallet password'
+                  />
+
+                  <Button disabled={isBusy}
+                    fluid
+                    type='submit' >
+                    Continue
+                  </Button>
+                </form>
+              </FormProvider>
+            </Box>
+
             <div>{name}</div>
             {
               !!genesis &&
@@ -162,7 +229,7 @@ function ImportLedger (): React.ReactElement {
           }
         </>
       }
-    </Box>
+    </>
   );
 }
 
