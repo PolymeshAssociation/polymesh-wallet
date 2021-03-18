441226795df3920965146c7c2f644b059e5fbe33
Remon Nashid <remon.sherin@gmail.com>
2021-03-18 15:41:01 -0700

    Format address in JSON import screen

--- packages/ui/src/Popup/ImportJson/UploadVerifyJson.tsx
+++ packages/ui/src/Popup/ImportJson/UploadVerifyJson.tsx
@@ -1,5 +1,6 @@
 import { KeyringPair$Json } from '@polkadot/keyring/types';
 import { hexToU8a, isHex, u8aToString } from '@polkadot/util';
+import { recodeAddress } from '@polymathnetwork/extension-core/utils';
 import { SvgDeleteOutline, SvgFileLockOutline } from '@polymathnetwork/extension-ui/assets/images/icons';
 import { Box, Button, ButtonSmall, Flex, Header, Icon, LabelWithCopy, Text, TextEllipsis, TextInput } from '@polymathnetwork/extension-ui/ui';
 import verifyJsonPassword from '@polymathnetwork/extension-ui/util/verifyJsonPassword';
@@ -7,7 +8,7 @@ import React, { FC, useContext, useRef, useState } from 'react';
 import { useErrorHandler } from 'react-error-boundary';
 import { FormProvider, useForm } from 'react-hook-form';
 
-import { ActivityContext } from '../../components';
+import { ActivityContext, PolymeshContext } from '../../components';
 import { jsonGetAccountInfo } from '../../messaging';
 
 type Props = {
@@ -28,6 +29,7 @@ export const UploadVerifyJson: FC<Props> = ({ onContinue }) => {
   const { clearErrors, errors, handleSubmit, register, setError } = methods;
   const isBusy = useContext(ActivityContext);
   const handleError = useErrorHandler();
+  const { networkState: { ss58Format } } = useContext(PolymeshContext);
 
   const onSubmit = (data: { [x: string]: string; }) => {
     if (accountJson) {
@@ -216,7 +218,7 @@ export const UploadVerifyJson: FC<Props> = ({ onContinue }) => {
                     </Flex>
                   </Flex>
                   <LabelWithCopy color='gray.3'
-                    text={accountJson?.address || ''}
+                    text={accountJson?.address ? recodeAddress(accountJson.address, ss58Format) : ''}
                     textSize={30}
                     textVariant='b3'
                   />
