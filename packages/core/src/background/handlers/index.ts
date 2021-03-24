52543eaf6ade940d0586014cb9585a69c20534a0
Minh <hlminh2000@gmail.com>
2021-03-24 15:42:35 -0400

    ♻️ refactors to use common PORTS constant

--- packages/core/src/background/handlers/index.ts
+++ packages/core/src/background/handlers/index.ts
@@ -1,5 +1,5 @@
-import { PORT_EXTENSION } from '@polkadot/extension-base/defaults';
 import { assert } from '@polkadot/util';
+import { PORTS } from '@polymathnetwork/extension-core/constants';
 
 import { PolyMessageTypes, PolyTransportRequestMessage } from '../types';
 import Extension from './Extension';
@@ -11,7 +11,7 @@ const extension = new Extension(state); // handles messages coming from the exte
 const tabs = new Tabs(state); // handles messages coming from the app running in the currently open tab
 
 export default function polyHandler<TMessageType extends PolyMessageTypes> ({ id, message, request }: PolyTransportRequestMessage<TMessageType>, port: chrome.runtime.Port): void {
-  const isExtension = port.name === `polywallet_${PORT_EXTENSION}`;
+  const isExtension = port.name === PORTS.EXTENSION;
   const sender = port.sender as chrome.runtime.MessageSender;
   const from = isExtension
     ? 'extension'
--- packages/extension/src/page.ts
+++ packages/extension/src/page.ts
@@ -1,23 +1,20 @@
 import { handleResponse } from '@polkadot/extension-base/page';
 import { Message } from '@polkadot/extension-base/types';
 import { injectExtension } from '@polkadot/extension-inject';
+import { PORTS } from '@polymathnetwork/extension-core/constants';
 import { enable } from '@polymathnetwork/extension-core/page';
 
 // setup a response listener (events created by the loader for extension responses)
 window.addEventListener('message', (message: Message): void => {
   const { data, source } = message
   // only allow messages from our window, by the loader
-  if (source !== window || data.origin !== 'content') {
+  if (source !== window || data.origin !== PORTS.CONTENT) {
     return;
   }
 
-  console.log("message received in page: ", message.data)
-
   if (data.id) {
-    if ((data as any).from === 'polywallet') {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      handleResponse(data as any);
-    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    handleResponse(data as any);
   } else {
     console.error('Missing id for response.');
   }
