de3bc6f08fcfeed9551d5e9d8766fa2f9b09541c
Wahid Rahim <wahidrahim@gmail.com>
2021-04-14 21:04:22 -0400

    refactor and restrict account details grid

--- packages/ui/src/ui/TextOverflowEllipsis/TextOverflowEllipsis.tsx
+++ packages/ui/src/ui/TextOverflowEllipsis/TextOverflowEllipsis.tsx
@@ -2,14 +2,14 @@ import { Text } from '@polymathnetwork/extension-ui/ui';
 import React, { ComponentProps, FC } from 'react';
 import styled from 'styled-components';
 
-export const EllipsiedText = styled(Text)<{ maxWidth: number | string }>`
+export const EllipsiedText = styled(Text)`
   display: inline-block;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
 `;
 
-type Props = { maxWidth: number | string } & ComponentProps<typeof Text>;
+type Props = { maxWidth?: number | string } & ComponentProps<typeof Text>;
 
 export const TextOverflowEllipsis: FC<Props> = ({ children, ...textProps }) => (
   <EllipsiedText {...textProps}>{children}</EllipsiedText>
