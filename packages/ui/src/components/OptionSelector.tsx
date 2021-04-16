05ce30d04363c5decc377c077d1b83a9a6270fd2
Wahid Rahim <wahidrahim@gmail.com>
2021-04-16 16:48:10 -0400

    render option selector component with provided options

---
+++ packages/ui/src/components/OptionSelector.tsx
@@ -0,0 +1,65 @@
+import { Flex, Text } from '@polymathnetwork/extension-ui/ui';
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+type OptionLabel = string | JSX.Element;
+
+export type Option = {
+  label: OptionLabel;
+  value: any;
+};
+
+type OptionSelectorProps = PropsWithChildren<{
+  options: Option[];
+}>;
+
+export function OptionSelector (props: OptionSelectorProps): JSX.Element {
+  const { children, options } = props;
+
+  const renderOption = (label: OptionLabel) => {
+    return typeof label === 'string'
+      ? (
+        <Flex className='network-item'
+          px='16px'
+          py='8px'>
+          <Text variant='b2m'>
+            {label}
+          </Text>
+        </Flex>
+      )
+      : label;
+  };
+
+  return (
+    <Wrapper>
+      {children}
+      <Options>
+        <ul>
+          {options.map((option, index) => (
+            <li key={index}>
+              {renderOption(option.label)}
+            </li>
+          ))}
+        </ul>
+      </Options>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Options = styled.div`
+  position: absolute;
+  background: white;
+  box-shadow: ${(props) => props.theme.shadows[3]};
+  border-radius: 8px;
+  padding: 8px 0;
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+`;
