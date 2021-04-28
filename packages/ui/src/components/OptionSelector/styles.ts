aabc03f651b7b7f91f64c707b381ab15e1231d2b
Wahid Rahim <wahidrahim@gmail.com>
2021-04-28 00:58:53 -0400

    refactor to split component, reducer, style, and types in multiple files

---
+++ packages/ui/src/components/OptionSelector/styles.ts
@@ -0,0 +1,33 @@
+import { styled } from '@polymathnetwork/extension-ui/styles';
+import { ThemeProps } from '@polymathnetwork/extension-ui/types';
+import { Box } from '@polymathnetwork/extension-ui/ui';
+
+import { CssPosition } from './types';
+
+export const Options = styled(Box)<{ cssPosition: CssPosition }>`
+  position: absolute;
+  background: white;
+  box-shadow: ${({ theme }: ThemeProps) => theme.shadows[3]};
+  border-radius: 8px;
+  padding: 8px 0;
+  z-index: 1000;
+
+  ${({ cssPosition }) => cssPosition}
+
+  visibility: ${({ cssPosition }) => (Object.values(cssPosition).length ? 'visible' : 'hidden')};
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+
+    li {
+      cursor: pointer;
+      white-space: nowrap;
+
+      &:hover {
+        background: ${({ theme }: ThemeProps) => theme.colors.gray[4]};
+      }
+    }
+  }
+`;
