51fe50c4e87c26372c90aefe7e14b1f8bd74973f
Wahid Rahim <wahidrahim@gmail.com>
2021-09-10 19:07:16 -0400

    refactor: update uid unlock ui

--- packages/ui/src/Popup/AccountDetails/UnlockUid.tsx
+++ packages/ui/src/Popup/AccountDetails/UnlockUid.tsx
@@ -27,43 +27,40 @@ export const DecodeUid: FC<Props> = ({ decode, onClose }) => {
   return (
     <>
       <Flex justifyContent='space-between'
-        mx='s'
-        my='s'>
+        m='m'>
         <Box>
-          <Text color='gray.1'
-            variant='c2'>
-            SHOW MY DID
+          <Text color='gray1'
+            variant='c1'>
+            SHOW MY uID
           </Text>
         </Box>
-        <Box>
-          <Icon
-            Asset={SvgClose}
-            color='gray.3'
-            height={14}
-            onClick={onClose}
-            style={{ cursor: 'pointer' }}
-            width={14}
-          />
-        </Box>
+        <Icon
+          Asset={SvgClose}
+          color='gray5'
+          height={14}
+          onClick={onClose}
+          style={{ cursor: 'pointer' }}
+          width={14}
+        />
       </Flex>
       <Flex flex={1}
         flexDirection='column'
         justifyContent='space-between'
-        mx='s'>
+        mx='m'>
         <Box>
           <Box>
-            <Text color='gray.2'
+            <Text color='gray3'
               variant='b2'>
               Your uID is a unique identifier attached to a single person or entity in the real world. A person or entity can have multiple Polymesh Accounts, but only one uID.
             </Text>
           </Box>
-          <Box mt='m'>
+          <Box mt='l'>
             <Text color='gray.1'
               variant='b2m'>
               Password
             </Text>
           </Box>
-          <Box mt='s'>
+          <Box mt='xs'>
             <form id='passwordForm'
               onSubmit={handleSubmit(onSubmit)}>
               <TextInput
@@ -95,7 +92,7 @@ export const DecodeUid: FC<Props> = ({ decode, onClose }) => {
               </Button>
             </Flex>
             <Flex flex={1}
-              ml='xs'>
+              ml='8px'>
               <Button fluid
                 form='passwordForm'
                 type='submit'>
