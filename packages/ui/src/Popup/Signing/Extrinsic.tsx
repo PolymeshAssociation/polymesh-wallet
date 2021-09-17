8bac717a795ed5343afe6ba96e507822ad293767
Wahid Rahim <wahidrahim@gmail.com>
2021-09-16 21:13:20 -0400

    fix: temporarily disable network mismatch detection

--- packages/ui/src/Popup/Signing/Extrinsic.tsx
+++ packages/ui/src/Popup/Signing/Extrinsic.tsx
@@ -22,49 +22,50 @@ function Extrinsic ({ request }: Props): React.ReactElement<Props> {
   const [callDetails, setCallDetails] = useState<ResponsePolyCallDetails>();
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const targetNetwork = genesisToNetworkName(request.genesisHash);
-    const networkMismatch = targetNetwork !== selectedNetwork;
-    const selectedLabel = networkLabels[selectedNetwork];
-    const targetLabel = !!targetNetwork && targetNetwork in networkLabels
-      ? networkLabels[targetNetwork]
-      : 'Unknown network';
-    const msg = `"${selectedLabel}" is selected on wallet while submitting an extrinsic to "${targetLabel}".
-    Network and Protocol fee estimation might not be accurate.`;
+  // @TODO: determine how to detect network mismatch differently. Since genesis hash can be the same for different networks.
+  // useEffect(() => {
+  //   const targetNetwork = genesisToNetworkName(request.genesisHash);
+  //   const networkMismatch = targetNetwork !== selectedNetwork;
+  //   const selectedLabel = networkLabels[selectedNetwork];
+  //   const targetLabel = !!targetNetwork && targetNetwork in networkLabels
+  //     ? networkLabels[targetNetwork]
+  //     : 'Unknown network';
+  //   const msg = `"${selectedLabel}" is selected on wallet while submitting an extrinsic to "${targetLabel}".
+  //   Network and Protocol fee estimation might not be accurate.`;
 
-    if (networkMismatch) {
-      toast.error(
-        <Flex alignItems='flex-start'
-          flexDirection='column'>
-          <Flex>
-            <Icon Asset={SvgAlertCircle}
-              color='yellow.0'
-              height={20}
-              width={20} />
-            <Box ml='s'>
-              <Text color='white'
-                variant='b1m'>
-                Network mismatch detected
-              </Text>
-            </Box>
-          </Flex>
-          <Box mt='4px'>
-            <Text color='gray.4'
-              variant='b3'>
-              {msg}
-            </Text>
-          </Box>
-        </Flex>, {
-          toastId,
-          hideProgressBar: true,
-          closeButton: true,
-          autoClose: false
-        }
-      );
-    } else {
-      toast.dismiss(toastId);
-    }
-  }, [request, selectedNetwork]);
+  //   if (networkMismatch) {
+  //     toast.error(
+  //       <Flex alignItems='flex-start'
+  //         flexDirection='column'>
+  //         <Flex>
+  //           <Icon Asset={SvgAlertCircle}
+  //             color='yellow.0'
+  //             height={20}
+  //             width={20} />
+  //           <Box ml='s'>
+  //             <Text color='white'
+  //               variant='b1m'>
+  //               Network mismatch detected
+  //             </Text>
+  //           </Box>
+  //         </Flex>
+  //         <Box mt='4px'>
+  //           <Text color='gray.4'
+  //             variant='b3'>
+  //             {msg}
+  //           </Text>
+  //         </Box>
+  //       </Flex>, {
+  //         toastId,
+  //         hideProgressBar: true,
+  //         closeButton: true,
+  //         autoClose: false
+  //       }
+  //     );
+  //   } else {
+  //     toast.dismiss(toastId);
+  //   }
+  // }, [request, selectedNetwork]);
 
   useEffect(() => {
     setLoading(true);
