cac0e4b359cf23e1e387acf94ae32e71c985c6ac
Wahid Rahim <wahidrahim@gmail.com>
2021-09-28 15:44:57 -0400

    feat: semantic-release

---
+++ release.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  branches: [
+    'master',
+    {
+      name: 'develop',
+      prerelease: true
+    },
+    {
+      name: 'semantic-release',
+      prerelease: true
+    }
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'master-build.zip',
+            label: '${nextRelease.version}.zip' // eslint-disable-line no-template-curly-in-string
+          }
+        ]
+      }
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' // eslint-disable-line no-template-curly-in-string
+      }
+    ]
+  ]
+};
