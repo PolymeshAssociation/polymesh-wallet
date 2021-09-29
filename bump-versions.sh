eb26b35df9344a424390699cbfc8482b43a43d6d
Wahid Rahim <wahidrahim@gmail.com>
2021-09-29 01:19:55 -0400

    fix: update bump-versions script

--- bump-versions.sh
+++ bump-versions.sh
@@ -1,15 +1,8 @@
 git pull origin $(git rev-parse --abbrev-ref HEAD)
 
-yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
-cd packages/core
-yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
-cd ../extension
-yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
-cd ../ui
-yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
-cd ../..
-yarn build
+yarn workspace @polymathnetwork/extension-core version --no-git-tag-version --no-commit-hooks --new-version ${1}
+yarn workspace @polymathnetwork/extension-ui version --no-git-tag-version --no-commit-hooks --new-version ${1}
+yarn workspace @polymathnetwork/extension version --no-git-tag-version --no-commit-hooks --new-version ${1}
+yarn workspace @polymathnetwork/extension add @polymathnetwork/extension-core@${1} @polymathnetwork/extension-ui@${1} 
 
-git add .
-git commit -am "chore(release): update package versions ${1} [skip ci]"
-git push
+yarn build
