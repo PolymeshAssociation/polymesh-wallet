51b95fdbeee0ed983c6803df38e6f7f4effd6ce8
Wahid Rahim <wahidrahim@gmail.com>
2021-09-29 00:38:05 -0400

    fix: test script update

--- bump-versions.sh
+++ bump-versions.sh
@@ -1,2 +1,9 @@
-npm version --workspaces ${1}
+yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
+cd packages/core
+yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
+cd ../extension
+yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
+cd ../ui
+yarn version --no-git-tag-version --no-commit-hooks --new-version ${1}
+cd ../..
 yarn build
--- commit-versions.sh
+++ commit-versions.sh
@@ -1,3 +1,3 @@
-git add .
-git commit -am "chore(release): update package versions ${1} [skip ci]"
-git push
+# git add .
+# git commit -am "chore(release): update package versions ${1} [skip ci]"
+# git push
