24116b73a9faa7e25f255f04d6e1ca1887356080
Wahid Rahim <wahidrahim@gmail.com>
2021-11-01 17:12:24 -0400

    chore(release): update changelog commit message

--- release.config.js
+++ release.config.js
@@ -26,7 +26,7 @@ module.exports = {
       '@semantic-release/git',
       {
         assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        message: 'chore(release): add changelog entry for version ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ]
   ]
