a437f4c26da1de6194e7f076736dd3a195957ee4
Wahid Rahim <wahidrahim@gmail.com>
2021-11-05 11:01:25 -0400

    fix: disable uid whitelist checks (#227)
    * fix: disable uid whitelist checks
    
    * chore: fix lint

--- packages/core/src/background/handlers/Tabs.ts
+++ packages/core/src/background/handlers/Tabs.ts
@@ -9,7 +9,7 @@ import polyNetworkSubscribe from '@polymathnetwork/extension-core/external/polyN
 import { getSelectedAccount, getSelectedIdentifiedAccount } from '@polymathnetwork/extension-core/store/getters';
 import { subscribeSelectedAccount } from '@polymathnetwork/extension-core/store/subscribers';
 import { NetworkMeta, ProofRequestPayload, RequestPolyProvideUid } from '@polymathnetwork/extension-core/types';
-import { allowedUidProvider, allowedUidReader, prioritize, recodeAddress, validateNetwork, validateSelectedNetwork, validateTicker, validateUid } from '@polymathnetwork/extension-core/utils';
+import { prioritize, recodeAddress, validateNetwork, validateSelectedNetwork, validateTicker, validateUid } from '@polymathnetwork/extension-core/utils';
 
 import { Errors, PolyMessageTypes, PolyRequestTypes, PolyResponseTypes, ProofingResponse, ReadUidResponse, RequestPolyReadUid } from '../types';
 import State from './State';
@@ -66,11 +66,11 @@ export default class Tabs extends DotTabs {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  private _accountsList (url: string): InjectedAccount[] {
+  private _accountsList (): InjectedAccount[] {
     return transformAccounts(accountsObservable.subject.getValue());
   }
 
-  private _accountsSubscribe (url: string, id: string, port: chrome.runtime.Port): boolean {
+  private _accountsSubscribe (id: string, port: chrome.runtime.Port): boolean {
     const cb = createSubscription<'pub(accounts.subscribe)'>(id, port);
     let calls = 0;
 
@@ -117,7 +117,8 @@ export default class Tabs extends DotTabs {
   }
 
   private provideUid (url: string, request: RequestPolyProvideUid): Promise<boolean> {
-    assert(allowedUidProvider(url), `App ${url} is not allowed to provide uid`);
+    // XXX: felt unnecessary, might delete later
+    // assert(allowedUidProvider(url), `App ${url} is not allowed to provide uid`);
 
     const { network, uid } = request;
 
@@ -138,7 +139,8 @@ export default class Tabs extends DotTabs {
   }
 
   private readUid (url: string, request: RequestPolyReadUid): Promise<ReadUidResponse> {
-    assert(allowedUidReader(url), `App ${url} is not allowed access uid`);
+    // XXX: felt unnecessary, might delete later
+    // assert(allowedUidReader(url), `App ${url} is not allowed access uid`);
 
     const account = getSelectedIdentifiedAccount();
 
@@ -175,10 +177,10 @@ export default class Tabs extends DotTabs {
         return this.polyNetworkSubscribe(id, port);
 
       case 'pub(accounts.list)':
-        return this._accountsList(url);
+        return this._accountsList();
 
       case 'pub(accounts.subscribe)':
-        return this._accountsSubscribe(url, id, port);
+        return this._accountsSubscribe(id, port);
 
       case 'pub(metadata.list)': {
         // Deny app's request to provide metadata because Polymesh wallet
