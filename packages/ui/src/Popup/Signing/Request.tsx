fcd40c804800ce9f5c000e74272c5231de1333d8
Wahid Rahim <wahidrahim@gmail.com>
2021-12-01 01:14:51 -0500

    fix: provide prop

--- packages/ui/src/Popup/Signing/Request.tsx
+++ packages/ui/src/Popup/Signing/Request.tsx
@@ -192,6 +192,7 @@ export default function Request({
           rejectOnly={!!isHardware}
           setError={setError}
           signId={signId}
+          isAbleToPay={isAbleToPay}
         />
       </>
     );
