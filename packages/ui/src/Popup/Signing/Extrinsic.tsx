dbf68791513ef95d42ec99f803987aaac91539b1
Wahid Rahim <wahidrahim@gmail.com>
2021-12-01 20:37:55 -0500

    chore: delete commented out code

--- packages/ui/src/Popup/Signing/Extrinsic.tsx
+++ packages/ui/src/Popup/Signing/Extrinsic.tsx
@@ -1,28 +1,13 @@
 import React from 'react';
-// import { SignerPayloadJSON } from '@polkadot/types/types';
 import { ResponsePolyCallDetails } from '@polymathnetwork/extension-core/background/types';
-// import { networkLabels } from '@polymathnetwork/extension-core/constants';
-// import { SvgAlertCircle } from '@polymathnetwork/extension-ui/assets/images/icons';
-// import { PolymeshContext } from '@polymathnetwork/extension-ui/components';
-// import { getPolyCallDetails } from '@polymathnetwork/extension-ui/messaging';
-// import { Box, Flex, Icon, Loading, Text } from '@polymathnetwork/extension-ui/ui';
 import { Flex, Loading } from '@polymathnetwork/extension-ui/ui';
-// import { genesisToNetworkName } from '@polymathnetwork/extension-ui/util/chains';
-
-// import { toast } from 'react-toastify';
 import Method from './Method';
-
-// const toastId = 'network-mismatch';
-
 interface Props {
   callDetails?: ResponsePolyCallDetails;
   loading: boolean;
 }
 
 function Extrinsic({ callDetails, loading }: Props): React.ReactElement<Props> {
-  // const { networkState: { selected: selectedNetwork } } = useContext(PolymeshContext);
-  // const [callDetails, setCallDetails] = useState<ResponsePolyCallDetails>();
-
   // @TODO: determine how to detect network mismatch differently. Since genesis hash can be the same for different networks.
   // useEffect(() => {
   //   const targetNetwork = genesisToNetworkName(request.genesisHash);
@@ -67,19 +52,6 @@ function Extrinsic({ callDetails, loading }: Props): React.ReactElement<Props> {
   //     toast.dismiss(toastId);
   //   }
   // }, [request, selectedNetwork]);
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   getPolyCallDetails(request)
-  //     .then((callDetails) => {
-  //       setCallDetails(callDetails);
-  //       setLoading(false);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //       setLoading(false);
-  //     });
-  // }, [request]);
 
   return (
     <>
