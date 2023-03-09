7767afaa47f17684616eb1d0ddc8e83e81acde6d
F-OBrien <francis@polymesh.network>
2023-03-09 20:35:21 +0000

    fix: adjust window to desired inner dimensions

--- packages/ui/src/createView.tsx
+++ packages/ui/src/createView.tsx
@@ -13,7 +13,20 @@ export default function createView(
   // First thing, we set window size because the default window size is too wide.
   // There's hardly any clean way to accomplish this, since window size is set initially
   // via chrome.windows.create, which is invoked by Polkadot's singing request handling.
-  window.resizeTo(400, 621);
+  const targetInnerWidth = 400;
+  const targetInnerHeight = 600;
+
+  if (
+    window.innerWidth !== targetInnerWidth ||
+    window.innerHeight !== targetInnerHeight
+  ) {
+    const targetOuterWidth =
+      targetInnerWidth + (window.outerWidth - window.innerWidth);
+    const targetOuterHeight =
+      targetInnerHeight + (window.outerHeight - window.innerHeight);
+
+    window.resizeTo(targetOuterWidth, targetOuterHeight);
+  }
 
   const rootElement = document.getElementById(rootId);
 
