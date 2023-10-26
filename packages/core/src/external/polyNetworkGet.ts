17c150372a8f96ad1da87e8e12f898b227ca881a
F-OBrien <francis@polymesh.network>
2023-10-26 16:48:55 +0100

    feat: return wssUrl for custom networks

--- packages/core/src/external/polyNetworkGet.ts
+++ packages/core/src/external/polyNetworkGet.ts
@@ -1,13 +1,18 @@
 import { networkLabels, networkURLs } from '../constants';
-import { getNetwork } from '../store/getters';
-import { NetworkMeta } from '../types';
+import { getNetwork, getCustomNetworkUrl } from '../store/getters';
+import { NetworkMeta, NetworkName } from '../types';
 
 export default function (): NetworkMeta {
   const network = getNetwork();
+  let wssUrl = networkURLs[network];
+
+  if (network === NetworkName.custom) {
+    wssUrl = getCustomNetworkUrl();
+  }
 
   return {
     name: network,
     label: networkLabels[network],
-    wssUrl: networkURLs[network],
+    wssUrl,
   };
 }
--- packages/core/src/external/polyNetworkSubscribe.ts
+++ packages/core/src/external/polyNetworkSubscribe.ts
@@ -2,19 +2,57 @@ import { Unsubscribe } from '@reduxjs/toolkit';
 
 import { networkLabels, networkURLs } from '../constants';
 import reduxSubscribe from '../store/reduxSubscribe';
-import { selectedNetwork } from '../store/selectors';
+import { customNetworkUrl, selectedNetwork } from '../store/selectors';
 import { NetworkMeta, NetworkName } from '../types';
+import { getCustomNetworkUrl, getNetwork } from '../store/getters';
 
 export default function (cb: (networkMeta: NetworkMeta) => void): Unsubscribe {
-  const innerCb = (networkName: NetworkName) => {
+  let firstCall = true;
+
+  const onNetworkNameUpdate = (networkName: NetworkName) => {
+    let wssUrl = networkURLs[networkName];
+
+    if (networkName === NetworkName.custom) {
+      wssUrl = getCustomNetworkUrl();
+    }
+
     const networkMeta = {
       name: networkName,
       label: networkLabels[networkName],
-      wssUrl: networkURLs[networkName],
+      wssUrl,
     };
 
     cb(networkMeta);
   };
 
-  return reduxSubscribe(selectedNetwork, innerCb);
+  const onCustomUrlUpdate = (customUrl: string) => {
+    // Skip the first callback so the subscription doesn't return twice initially.
+    if (firstCall) {
+      firstCall = false;
+      return;
+    }
+    const networkName = getNetwork();
+    let wssUrl = networkURLs[networkName];
+    if (networkName === NetworkName.custom) {
+      wssUrl = customUrl;
+    }
+
+    const networkMeta = {
+      name: networkName,
+      label: networkLabels[networkName],
+      wssUrl,
+    };
+
+    cb(networkMeta);
+  };
+
+  const networkUnsub = reduxSubscribe(selectedNetwork, onNetworkNameUpdate);
+  const customUrlUnsub = reduxSubscribe(customNetworkUrl, onCustomUrlUpdate);
+
+  const unsubAll: Unsubscribe = () => {
+    customUrlUnsub();
+    networkUnsub();
+  };
+
+  return unsubAll;
 }
