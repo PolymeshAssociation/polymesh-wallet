42557ef29855931aa209cf6bfeae939f3aa2c703
F-OBrien <francis@polymesh.network>
2023-11-06 12:10:24 +0000

    fix: remove network specific account states

--- packages/core/src/store/features/accounts.ts
+++ packages/core/src/store/features/accounts.ts
@@ -1,75 +1,38 @@
-/* eslint-disable sort-keys */
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import merge from 'lodash/merge';
 import isEqual from 'lodash-es/isEqual';
 
-import { AccountData, NetworkName } from '../../types';
-import { initialFeatureState } from '../utils';
+import { AccountData } from '../../types';
 
-type State = { selected?: string } & Record<
-  NetworkName,
-  Record<string, AccountData>
->;
-const initialState: State = initialFeatureState() as Record<
-  NetworkName,
-  Record<string, AccountData>
->;
-
-type SetAccountPayload = { network: NetworkName; data: AccountData };
-type RemoveAccountPayload = { network: NetworkName; address: string };
-type SelectAccountPayload = string;
+type AccountsState = { selected?: string; keys: Record<string, AccountData> };
+const initialState: AccountsState = {
+  keys: {},
+} as AccountsState;
 
 const accountsSlice = createSlice({
   name: 'accounts',
   initialState,
   reducers: {
-    setAccount(state, action: PayloadAction<SetAccountPayload>) {
-      const { data, network } = action.payload;
-      const prev = state[network][data.address];
+    setAccount(state, action: PayloadAction<AccountData>) {
+      const data = action.payload;
+      const prev = state.keys[data.address];
       const next = merge(prev, data);
 
       if (!isEqual(next, prev)) {
-        state[network][data.address] = next;
+        state.keys[data.address] = next;
       }
     },
-    removeAccount(state, action: PayloadAction<RemoveAccountPayload>) {
-      const { address, network } = action.payload;
-
-      delete state[network][address];
-
-      if (state.selected === address) {
-        state.selected = undefined;
-      }
-    },
-    selectAccount(state, action: PayloadAction<SelectAccountPayload>) {
-      state.selected = action.payload;
-    },
-    setAccountGlobally(state, action: PayloadAction<AccountData>) {
-      const data = action.payload;
-
-      Object.keys(NetworkName).forEach((network: string) => {
-        const prev = state[network as NetworkName][data.address];
-        const next = merge(prev, data);
-
-        if (!isEqual(next, prev)) {
-          state[network as NetworkName][data.address] = next;
-        }
-      });
-    },
-    removeAccountGlobally(state, action: PayloadAction<string>) {
+    removeAccount(state, action: PayloadAction<string>) {
       const address = action.payload;
 
-      Object.keys(NetworkName).forEach((network: string) => {
-        const prev = state[network as NetworkName][address];
-
-        if (prev) {
-          delete state[network as NetworkName][address];
-        }
-      });
+      delete state.keys[address];
 
       if (state.selected === address) {
         state.selected = undefined;
       }
+    },
+    selectAccount(state, action: PayloadAction<string>) {
+      state.selected = action.payload;
     },
   },
 });
