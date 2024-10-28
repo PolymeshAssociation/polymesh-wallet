7fc3bc34f8d7f3d6e1d4122623da0c6faa2108e2
Mehdi Raash <leaksin@gmail.com>
2024-10-28 15:55:26 -0400

    fix: update loading animation

--- packages/ui/src/ui/Loading/styles.ts
+++ packages/ui/src/ui/Loading/styles.ts
@@ -4,65 +4,20 @@ import type { LoadingProps } from './types';
 import styled, { keyframes } from 'styled-components';
 
 const spin = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(359deg);
-  }
-`;
-
-const mask = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  25% {
-    transform: rotate(180deg);
-  }
-  50% {
-    transform: rotate(180deg);
-  }
-  75% {
-    transform: rotate(360deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
 `;
 
 export const Wrapper = styled.div<LoadingProps>`
   position: relative;
   width: ${({ small }) => (small ? '18px' : '64px')};
   height: ${({ small }) => (small ? '18px' : '64px')};
-  animation: 1s ${spin} infinite cubic-bezier(0.255, 0.2, 0.315, 0.455);
-  span,
-  span:before {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: ${({ small }) => (small ? '9px' : '32px')};
-    height: ${({ small }) => (small ? '18px' : '64px')};
-    overflow: hidden;
-    box-sizing: border-box;
-    transform-origin: 100% 50%;
-    border-top-left-radius: 32px;
-    border-bottom-left-radius: 32px;
-  }
-  span:before {
-    content: '';
-    border-width: ${({ small }) => (small ? '2px' : '5px')};
-    border-color: ${({ theme }: ThemeProps) => theme.colors.primary};
-    border-style: solid;
-    border-right-color: transparent;
-    animation: 4s ${mask} infinite linear;
-  }
-  span:nth-child(1) {
-    transform: rotate(180deg);
-  }
-  span:nth-child(2) {
-    transform: rotate(360deg);
-    &:before {
-      animation-delay: 1s;
-    }
-  }
+  border: ${({ small }) => (small ? '2px' : '5px')} solid ${({ theme }: ThemeProps) => theme.colors.primary};
+  border-radius: 50%;
+  border-top-color: transparent;
+  animation: ${spin} 1s cubic-bezier(0.255, 0.2, 0.315, 0.455) infinite;   
 `;
