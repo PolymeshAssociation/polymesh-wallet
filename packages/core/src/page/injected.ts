e6edd1dfb67daa4122de4eedf3d2ecca475ee7c9
F-OBrien <francis@polymesh.network>
2024-12-03 20:10:31 +0000

    feat: remove the ping messages from PolymeshInjected

--- packages/core/src/page/injected.ts
+++ packages/core/src/page/injected.ts
@@ -1,16 +1,25 @@
+import type { Injected } from '@polkadot/extension-inject/types';
 import type { SendRequest } from '@polymeshassociation/extension-core/page/types';
 
-import Injected from '@polkadot/extension-base/page/Injected';
+import Accounts from '@polkadot/extension-base/page/Accounts';
+import Metadata from '@polkadot/extension-base/page/Metadata';
+import PostMessageProvider from '@polkadot/extension-base/page/PostMessageProvider';
+import Signer from '@polkadot/extension-base/page/Signer';
 
 import Network from './Network';
 
-class PolymeshInjected extends Injected {
+export default class PolymeshInjected implements Injected {
+  public readonly accounts: Accounts;
+  public readonly metadata: Metadata;
   public readonly network: Network;
+  public readonly provider: PostMessageProvider;
+  public readonly signer: Signer;
 
   constructor (sendRequest: SendRequest) {
-    super(sendRequest);
+    this.accounts = new Accounts(sendRequest);
+    this.metadata = new Metadata(sendRequest);
     this.network = new Network(sendRequest);
+    this.provider = new PostMessageProvider(sendRequest);
+    this.signer = new Signer(sendRequest);
   }
 }
-
-export default PolymeshInjected;
