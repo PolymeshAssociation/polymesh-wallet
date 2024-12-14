bc0573a65520e798dd68ee362c62e030821aeb67
F-OBrien <francis@polymesh.network>
2024-12-14 18:12:04 +0000

    feat: enhance account migration process and ensure complete before loading keys

--- packages/core/src/store/accountMigrations.ts
+++ packages/core/src/store/accountMigrations.ts
@@ -12,15 +12,46 @@ export default class AccountMigrations extends BaseStore<KeyringJson> implements
   // EXTENSION_PREFIX configured in webpack.shared.cjs
   public async migrateUnPrefixedAccounts (): Promise<void> {
     const accountsStore = new AccountsStore();
+    const migrationPromises: Promise<void>[] = [];
 
     const migrateAccount = async (key: string, value: KeyringJson): Promise<void> => {
-      if (key.startsWith('account:')) {
-        await accountsStore.set(key, value);
+      let existingValue: KeyringJson | null = null;
+
+      await accountsStore.get(key, (value) => {
+        existingValue = value;
+      });
+
+      // If the account already exists, remove the old storage key
+      if (existingValue) {
+        console.log(`Account with key ${key} already migrated, removing the old storage key`);
+        await super.remove(key);
+
+        return;
+      }
+
+      // migrate the account to the new storage key
+      await accountsStore.set(key, value);
+
+      // Verify the migration was successful
+      await accountsStore.get(key, (value) => {
+        existingValue = value;
+      });
+
+      if (existingValue) {
+        // Migration successful, remove the old storage key
+        await super.remove(key);
+        console.log(`Migration successful for key ${key}`);
+      } else {
+        console.error(`Failed to verify migration for key ${key}`);
       }
     };
 
     await super.all((key, value): void => {
-      migrateAccount(key, value).catch(console.error);
+      if (key.startsWith('account:')) {
+        migrationPromises.push(migrateAccount(key, value).catch(console.error));
+      }
     });
+
+    await Promise.all(migrationPromises);
   }
 }
